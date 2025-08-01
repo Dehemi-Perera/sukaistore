8fc27cf036996c6d8814433a42bf3796a6ebaef7
Dehemi-Perera <lilyvihara07@gmail.com>
2025-08-01 23:07:44 +0530

    Initial fullstack project commit

---
+++ SukaiStore-UI/src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import ClassHeader from "./components/ClassHeader";
+import Footer from "./components/footer/Footer";
+import Home from "./components/Home";
+import React from "react";
+
+function App() {
+ return(
+  <>
+     <Header/>
+     <Home/>
+     <Footer/>
+  </>
+        
+ );
+}
+
+export default App
---
+++ SukaiStore-UI/src/components/PageHeading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PageTitle from "./PageTitle";
+
+export default function PageHeading()
+{
+  return (
+  <div className="page-heading-container">
+    <PageTitle title="Explore Our Beauty Products"/>;
+    </div>
+    
+    );
+  
+}
+
